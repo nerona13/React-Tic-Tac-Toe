f9281df1600c97f7203a7c1fb9fec8cddee8185e
Hector Nerona <hectornerona@gmail.com>
2024-07-19 16:36:46 -0700

    add header 'Vercel Automatic Deployment'

--- src/app/page.js
+++ src/app/page.js
@@ -100,6 +100,7 @@ export default function Game() {
 
   return (
     <div className="game">
+      <h1>Vercel Automatic Deployment</h1>
       <div className="game-board">
         <Board squares={currentSquares} xIsNext={xIsNext} onPlay={handlePlay} />
       </div>
